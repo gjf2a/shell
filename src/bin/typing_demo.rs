68dfd79cdb6871d9fc4e5206a7289b1d06626053
gjf2a <ferrer@hendrix.edu>
2023-02-01 09:22:15 -0600

    Fixed typing_demo.rs.

--- src/bin/typing_demo.rs
+++ src/bin/typing_demo.rs
@@ -18,7 +18,7 @@ fn main() {
 fn process_line() -> anyhow::Result<bool> {
     let mut user_input = String::new();
     print!("$ ");
-    std::io::stdout().flush().unwrap(); // Come back to this later....
+    std::io::stdout().flush()?; 
     std::io::stdin().read_line(&mut user_input)?;
     if user_input.trim() == "exit" {
         Ok(false)
