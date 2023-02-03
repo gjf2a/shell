fe7232cf9060d028ad3620b6b3743e4207d02eee
Gabriel Ferrer <ferrer@hendrix.edu>
2023-02-03 09:56:07 -0600

    Updated low_level_file.rs

--- src/bin/low_level_file.rs
+++ src/bin/low_level_file.rs
@@ -2,12 +2,13 @@ use nix::{fcntl::{OFlag, open}, sys::stat::Mode, unistd::{close, write}};
 
 fn main() -> anyhow::Result<()> {
     let mut flags = OFlag::empty();
-    flags.set(OFlag::O_CREAT, true);
-    flags.set(OFlag::O_WRONLY, true);
-    flags.set(OFlag::O_TRUNC, true);
-    let mut mode = Mode::empty();
-    mode.set(Mode::S_IRUSR, true);
-    mode.set(Mode::S_IWUSR, true);
+    for f in [OFlag::O_CREAT, OFlag::O_WRONLY, OFlag::O_TRUNC] {
+        flags.set(f, true);
+    }
+    let mut mode = Mode::all();
+    for m in [Mode::S_IXGRP, Mode::S_IXOTH, Mode::S_IXUSR] {
+        mode.set(m, false);
+    }
     let fd = open("test.txt", flags, mode)?;
     let text = "Hello, world!";
     let written = write(fd, text.as_bytes())?;
