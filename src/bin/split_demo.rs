38fd6143d4a6d2327c17a8399f5c76d3e9f035b6
Gabriel Ferrer <ferrer@hendrix.edu>
2023-02-15 16:05:38 -0600

    Added split_demo.rs

---
+++ src/bin/split_demo.rs
@@ -0,0 +1,18 @@
+fn main() {
+    for arg in std::env::args().skip(1) {
+        println!("Splitting {arg}");
+        let parts = splitter(arg.as_str(), ':');
+        for part in parts.iter() {
+            println!("{part}");
+        }
+    }
+    
+}
+
+fn splitter(s: &str, spl: char) -> Vec<String> {
+    let mut owned = vec![];
+    for part in s.split(spl) {
+        owned.push(part.to_owned());
+    }
+    owned
+}
